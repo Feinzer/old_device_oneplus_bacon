35857ee47ee4e11f890a36ac6d56bca141f22e31
Daniel Hernández <danhernan23i@gmail.com>
2019-11-25 03:54:38 -0400

    bacon: Change BOARD_VENDOR
    It should be oppo for us, not oneplus.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@
 -include device/oppo/msm8974-common/BoardConfigCommon.mk
 
 # Platform
-BOARD_VENDOR := oneplus
+BOARD_VENDOR := oppo
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
