7d372df2458dc082561094a39f0d39ba91b0a30e
Daniel Hernández <danhernan23i@gmail.com>
2020-01-15 16:02:41 -0400

    bacon: Add custom overlays to force default colors
    I hate Lineage's color choices. So I'd rather use the pixel ones.

--- bacon.mk
+++ bacon.mk
@@ -20,6 +20,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Custom Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-custom/pixel-colors # Use default pixel colors
+
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 19
 
